fafe42d906bba277804f90ba1a43b613ae62d87b
Sadikul Mobasshir <114331112+MOBASSHIR07@users.noreply.github.com>
2026-04-09 16:46:50 +0600

    debugging

--- app/(AuthLayout)/auth/callback/page.tsx
+++ app/(AuthLayout)/auth/callback/page.tsx
@@ -7,17 +7,19 @@ import { authClient } from "@/lib/auth-client";
 export default function AuthCallbackPage() {
   const router = useRouter();
 
- useEffect(() => {
+  useEffect(() => {
     const handleCallback = async () => {
+      await new Promise(resolve => setTimeout(resolve, 2000));
+      
       const session = await authClient.getSession();
-
+      console.log("Session response:", session);
+      
       if (!session?.data?.user) {
         console.error("No session found after callback");
         router.push("/sign-in");
         return;
       }
 
- 
       const res = await fetch("/api/set-session", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
@@ -30,18 +32,14 @@ export default function AuthCallbackPage() {
       }
 
       const role = session.data.user.role;
-
-      if (role === "ADMIN") {
-        router.push("/admin-dashboard");
-      } else if (role === "PROVIDER") {
-        router.push("/provider-dashboard");
-      } else {
-        router.push("/dashboard");
-      }
+      if (role === "ADMIN") router.push("/admin-dashboard");
+      else if (role === "PROVIDER") router.push("/provider-dashboard");
+      else router.push("/dashboard");
     };
 
     handleCallback();
   }, [router]);
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen">
       <div className="animate-spin rounded-full h-10 w-10 border-b-2 border-primary"></div>
